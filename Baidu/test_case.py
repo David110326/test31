3d7682d1513fd2e21e79d59cdd6a4e98a07d764c
David110326 <975298420@qq.com>
2023-03-15 18:39:09 +0800

    new project

---
+++ Baidu/test_case.py
@@ -0,0 +1,9 @@
+print("Start test********************************")
+from selenium.webdriver.chrome import webdriver
+from time import sleep
+driver=webdriver.WebDriver()
+driver.maximize_window()
+driver.get("https://www.baidu.com")
+print(driver.title)
+sleep(2)
+driver.quit()
