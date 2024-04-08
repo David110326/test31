4117bf9c78e3ae273cf88c60a9bb305eb6a7d173
David110326 <126149913+David110326@users.noreply.github.com>
2024-04-08 09:08:55 +0800

    Update test_case.py

--- Baidu/test_case.py
+++ Baidu/test_case.py
@@ -1,11 +1,9 @@
 print("Start test********************************")
 from selenium.webdriver.chrome import webdriver
 from time import sleep
-sleep(3)
 driver=webdriver.WebDriver()
 driver.maximize_window()
 driver.get("https://www.baidu.com")
-time.sleep(6)
 sleep(5)
 #add 
 #ffsfsfs
